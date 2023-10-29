274fb4c524a3c26960a99cab93ad8dc002e9b538
depressed <vshevtsov2022@gmail.com>
2023-10-30 01:00:45 +0200

    pre final

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173","http://192.168.1.106:5173","https://broker-test-final-functional.000webhostapp.com")
+                .allowedOrigins("https://broker-test-final-functional.000webhostapp.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
